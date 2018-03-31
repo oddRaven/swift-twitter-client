84c47ee07689791d2074feb3781f77ab0e6b8beb
Timo Hoff <timohoff@live.nl>
2018-03-31 12:04:05 +0200

    set up splash and login screens

--- TwitterClient/controllers/LoginViewController.swift
+++ TwitterClient/controllers/LoginViewController.swift
@@ -1,9 +1,9 @@
-//
-
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -13,7 +13,14 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func attemptSignIn(_ sender: UIButton) {
+        guard txtUsername.text! == "username" && txtPassword.text! == "password" else{
+            self.showToast("login mislukt")
+            return
+        }
+        
+        performSegue(withIdentifier: "signInSuccess", sender: nil)
+    }
 }
-
