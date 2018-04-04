82bc0e80c64766b84b7779da20f328839dcadb60
Timo Hoff <timohoff@live.nl>
2018-04-04 09:18:30 +0200

    oauth request token

--- TwitterClient/controllers/LoginViewController.swift
+++ TwitterClient/controllers/LoginViewController.swift
@@ -4,6 +4,11 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var txtUsername: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
     
+    let consumerKey = "jmsKXfZ7qHjxsdqTRtEwLiKy2"
+    let consumerSecret = "a56aLS53qu4G3zVrP9xBFI3rQnyy1RO8sBfdspXMm2SD71MxgT"
+    let accessToken = "1959289086-d1Z3On8y9WF7x1bl3iNub5JtJH2nkkNdVxvsUHJ"
+    let accessTokenSecret = "Oj3uCKUe7Guf5nzxeI8HYhvOAs5Q4BH6LmHp6mQzoW1iZ"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -14,8 +19,52 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    enum LocalError: Error {
+        case unknown
+    }
+    
+    func generateNonce(_ lenght: Int) throws -> String {
+        let data = NSMutableData(length: lenght)
+        let result = SecRandomCopyBytes(kSecRandomDefault, data!.length, data!.mutableBytes)
+        if result == errSecSuccess {
+            return String(describing: data!.base64EncodedString(options: NSData.Base64EncodingOptions(rawValue: 0))) // String(describing: nonce! as Data)
+        } else {
+            throw LocalError.unknown
+        }
+    }
     
     @IBAction func attemptSignIn(_ sender: UIButton) {
+        let urlString = "https://api.twitter.com/oauth/request_token"
+        if let tokenUrl = NSURL(string: urlString) {
+            let req = NSMutableURLRequest(url: tokenUrl as URL)
+            req.httpMethod = "POST"
+            /*guard let nonce = try? generateNonce(64) else{
+                return
+            }*/
+            let nonce = String(UUID().uuidString.prefix(8))
+            let message = consumerSecret + "&" + accessTokenSecret
+            let signature: String = message.hmac(algorithm: .SHA1, key: consumerKey)
+            let timestamp = String(Int64(Date().timeIntervalSince1970))
+            let authorizationHeader = "OAuth oauth_nonce=\"\(nonce)\", oauth_callback=\"oob\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"\(timestamp)\", oauth_consumer_key=\"\(consumerKey)\", oauth_signature=\"\(signature)\", oauth_version=\"1.0\""
+            print(authorizationHeader)
+            req.addValue(authorizationHeader, forHTTPHeaderField: "Authorization")
+           
+            let task = URLSession.shared.dataTask(with: req as URLRequest) { data, response, error in
+                if let json = data {
+                    do {
+                        print(response!)
+                        if let content = try JSONSerialization.jsonObject(with: json, options: []) as? [String: AnyObject] {
+                            print(content.keys)
+                            /*if let accessToken = content["access_token"] as? String {
+                                print(accessToken)
+                            }*/
+                        }
+                    } catch {}
+                }
+            }
+            task.resume()
+        }
+        
         guard txtUsername.text! == "username" && txtPassword.text! == "password" else{
             self.showToast("login mislukt")
             return
---
+++ TwitterClient/controllers/hmac-sha1.swift
@@ -0,0 +1,68 @@
+import Foundation
+import CommonCrypto
+
+enum CryptoAlgorithm {
+    case MD5, SHA1, SHA224, SHA256, SHA384, SHA512
+    
+    var HMACAlgorithm: CCHmacAlgorithm {
+        var result: Int = 0
+        switch self {
+        case .MD5:      result = kCCHmacAlgMD5
+        case .SHA1:     result = kCCHmacAlgSHA1
+        case .SHA224:   result = kCCHmacAlgSHA224
+        case .SHA256:   result = kCCHmacAlgSHA256
+        case .SHA384:   result = kCCHmacAlgSHA384
+        case .SHA512:   result = kCCHmacAlgSHA512
+        }
+        return CCHmacAlgorithm(result)
+    }
+    
+    var digestLength: Int {
+        var result: Int32 = 0
+        switch self {
+        case .MD5:      result = CC_MD5_DIGEST_LENGTH
+        case .SHA1:     result = CC_SHA1_DIGEST_LENGTH
+        case .SHA224:   result = CC_SHA224_DIGEST_LENGTH
+        case .SHA256:   result = CC_SHA256_DIGEST_LENGTH
+        case .SHA384:   result = CC_SHA384_DIGEST_LENGTH
+        case .SHA512:   result = CC_SHA512_DIGEST_LENGTH
+        }
+        return Int(result)
+    }
+}
+
+extension String {
+    
+    func hmac(algorithm: CryptoAlgorithm, key: String) -> String {
+        let stringData = self.data(using: String.Encoding.utf8)
+        let keyData = key.data(using: String.Encoding.utf8)
+        
+        let result = hmacGenerate(from: stringData!, withKey: keyData!, using: algorithm)
+        
+        var hash: String = ""
+        for i in 0..<algorithm.digestLength {
+            hash += String(format: "%02x", result[i])
+        }
+        
+        return hash
+        
+        //return result.base64EncodedString()
+    }
+    
+    private func hmacGenerate(from data: Data, withKey key: Data, using algorithm: CryptoAlgorithm) -> Data {
+        // Get data pointers
+        var result = Data(count: Int(algorithm.digestLength))
+        
+        print("result \(result)")
+        
+        data.withUnsafeBytes{(dataBytesPointer: UnsafePointer<UInt8>) -> Void in
+            key.withUnsafeBytes{(keyBytesPointer: UnsafePointer<UInt8>) -> Void in
+                result.withUnsafeMutableBytes{(resultBytesPointer: UnsafeMutablePointer<UInt8>) -> Void in
+                    CCHmac(algorithm.HMACAlgorithm, keyBytesPointer, key.count, dataBytesPointer, data.count, resultBytesPointer)
+                }
+            }
+        }
+        
+        return result
+    }
+}
